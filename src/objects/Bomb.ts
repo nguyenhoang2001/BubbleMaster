73f6a4a5e0b83fadaf2833d05a9403e1c0855670
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2022-07-14 12:44:13 +0700

    adding effects image and update image for particles

--- src/objects/Bomb.ts
+++ src/objects/Bomb.ts
@@ -11,37 +11,46 @@ export class Bomb extends ShootedBubble {
         this.scene.add.existing(this);
         this.name = 'Bomb';
         this.setDepth(DEPTH.BULLET);
-        this.setUpParticles();
-        
+        this.setUpParticles();        
     }
 
-    private setUpParticles() {
-        this.yellowFire = this.scene.add.particles('gamePlay32bit','effects/circle-1').setDepth(DEPTH.PARTICLE)
-        let emitter1 = this.yellowFire.createEmitter({
+    public setUpParticles() {
+        this.yellowFire = this.scene.add.particles('star').setDepth(DEPTH.PARTICLE)
+        let emiter1 = this.yellowFire.createEmitter({
             tint: 0xffd712,
-            speed: 20,
-            blendMode: 'ADD',
-            lifespan: 1000
+            alpha: {start:0.5, end: 0},
+            speed: 60,
+            gravityY: 100,
+            blendMode: 'SCREEN',
+            frequency: 10,
+            angle: {min: 0, max: 360},
+            lifespan: 500
         });
-        emitter1.startFollow(this,this.width/2 - 10,-(this.height/2 - 5));
-        this.redFire = this.scene.add.particles('gamePlay32bit','effects/light');
-        let emitter2 = this.redFire.createEmitter({
-            // tint: 0xff0000,
-            speed: 50,
-            blendMode: 'ADD',
-            lifespan: 1000
+        emiter1.startFollow(this,this.width/2 - 10,-(this.height/2 - 5));
+        this.redFire = this.scene.add.particles('light').setDepth(DEPTH.PARTICLE);
+        let emiter2 = this.redFire.createEmitter({
+            tint: 0xeb2f2f,
+            alpha: 0.5,
+            speed: 30,
+            blendMode: 'SCREEN',
+            lifespan: 1000,
         });
-        emitter2.startFollow(this,this.width/2 - 10,-(this.height/2 - 5));
+        emiter2.startFollow(this,this.width/2 - 10,-(this.height/2 - 5));
     }
 
-    public removeParticle() {
+    private removeParticle() {
         this.yellowFire.destroy();
         this.redFire.destroy();
     }
 
+    public removeVisualEffect() {
+        this.tail.removeFromDisplayList();
+        this.removeParticle();
+    }
+
     public update(...args: any[]): void {
         this.updateTailPosition();
-        this.setRotation(this.body.velocity.angle());
+        // this.setRotation(this.body.velocity.angle());
         if(this.body.velocity.y != 0 && this.tail.visible == false) {
             this.tail.setVisible(true);
             this.setScale(1.1,1);
